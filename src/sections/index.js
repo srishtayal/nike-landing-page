5e2892c76c2d5aab27ed43def33fe558486ea05c
Srishti Tayal <srishti.tayal.ug23@gmail.com>
2024-08-27 23:25:33 +0530

    nike landing page

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+import SpecialOffer from "./SpecialOffer";
+ 
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
